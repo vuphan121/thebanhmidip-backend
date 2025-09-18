57f0e0af38412f0aa2a9ca0b29a89f78b8c96275
vuphan121 <157292772+vuphan121@users.noreply.github.com>
2025-09-19 00:03:57 +0700

    Create article.ts

---
+++ src/function/article.ts
@@ -0,0 +1,58 @@
+import {SupabaseTable} from "../db/supabase_table";
+import 'dotenv/config';
+
+export type Article = {
+    id?: string;
+    title: string;
+    content: string;
+    summary: string;
+    image_name: string
+    created_at?: string;
+};
+
+
+const API_URL = process.env.SUPABASE_API_URL!;
+const API_KEY = process.env.SUPABASE_API_KEY!;
+
+
+async function saveArticle(
+    id: string | null,
+    title: string,
+    content: string,
+    summary: string,
+    image_name: string
+): Promise<void> {
+    const articleTable = new SupabaseTable("article", API_URL, API_KEY);
+    if (!id) {
+        const newArticle: Article = {
+            title: title,
+            content: content,
+            summary: summary,
+            image_name: image_name
+        };
+        const result = await articleTable.add_row(newArticle);
+        console.log('Inserted:', result);
+    } else {
+        const result = await articleTable.update_by_id(id, {
+            title,
+            content,
+        });
+        console.log('Updated:', result);
+    }
+}
+
+export async function get_article_data(): Promise<Pick<Article, 'id' | 'title' | 'content' | 'summary' | 'image_name' | 'created_at'>[]> {
+    const articleTable = new SupabaseTable<Article>('article', API_URL, API_KEY);
+    const allArticles = await articleTable.get_data();
+
+    return allArticles.map(({ id, title, content , summary, image_name, created_at}) => ({
+        id,
+        title,
+        content,
+        summary,
+        image_name,
+        created_at
+    }));
+}
+
+
