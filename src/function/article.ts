698fde1a8f8e8e591c15d949fa4edebe184ba209
Vu Phan <157292772+vuphan121@users.noreply.github.com>
2025-09-19 00:04:06 +0700

    Update article.ts

--- src/function/article.ts
+++ src/function/article.ts
@@ -1,68 +1,81 @@
-import { SupabaseTable } from "../db/supabase_table";
+import {SupabaseTable} from "../db/supabase_table";
 import 'dotenv/config';
 
 export type Article = {
-  id?: string;
-  title: string;
-  content: string;
-  summary: string;
-  image_name: string;
-  title_link?: string;
-  created_at?: string;
-  image_source_name?: string;
-  image_source_link?: string;
+    id?: string;
+    title: string;
+    content: string;
+    summary: string;
+    image_name: string;
+    issue_id?: string;
+    title_link?: string;
+    created_at?: string;
+    image_source_name?: string;
+    image_source_link?: string;
 };
+
 
 const API_URL = process.env.SUPABASE_API_URL!;
 const API_KEY = process.env.SUPABASE_API_KEY!;
 
-async function saveArticle(
-  id: string | null,
-  title: string,
-  content: string,
-  summary: string,
-  image_name: string,
-  title_link?: string
-): Promise<void> {
-  const articleTable = new SupabaseTable<Article>("article", API_URL, API_KEY);
 
-  if (!id) {
-    const newArticle: Article = {
-      title,
-      content,
-      summary,
-      image_name,
-      title_link
-    };
-    const result = await articleTable.add_row(newArticle);
-    console.log('Inserted:', result);
-  } else {
-    const result = await articleTable.update_by_id(id, {
-      title,
-      content,
-      title_link
-    });
-    console.log('Updated:', result);
-  }
+async function saveArticle(
+    id: string | null,
+    title: string,
+    content: string,
+    summary: string,
+    image_name: string
+): Promise<void> {
+    const articleTable = new SupabaseTable("article", API_URL, API_KEY);
+    if (!id) {
+        const newArticle: Article = {
+            title: title,
+            content: content,
+            summary: summary,
+            image_name: image_name
+        };
+        const result = await articleTable.add_row(newArticle);
+        console.log('Inserted:', result);
+    } else {
+        const result = await articleTable.update_by_id(id, {
+            title,
+            content,
+        });
+        console.log('Updated:', result);
+    }
 }
 
 export async function get_article_data(): Promise<
-  Pick<Article, 'id' | 'title' | 'summary' | 'image_name' | 'created_at'>[]
+    Pick<Article, 'id' | 'title' | 'summary' | 'image_name' | 'created_at'>[]
 > {
-  const articleTable = new SupabaseTable<Article>('article', API_URL, API_KEY);
-  const allArticles = await articleTable.get_data();
+    const articleTable = new SupabaseTable<Article>('article', API_URL, API_KEY)
+    const allArticles = await articleTable.get_data()
 
-  return allArticles.map(({ id, title, summary, image_name, created_at }) => ({
-    id,
-    title,
-    summary,
-    image_name,
-    created_at
-  }));
+    return allArticles.map(({ id, title, summary, image_name, created_at }) => ({
+        id,
+        title,
+        summary,
+        image_name,
+        created_at
+    }))
 }
 
 export async function get_article_by_id(id: string): Promise<Article | null> {
-  const articleTable = new SupabaseTable<Article>('article', API_URL, API_KEY);
-  const result = await articleTable.get_row_by_id(id);
-  return result ?? null;
+    const articleTable = new SupabaseTable<Article>('article', API_URL, API_KEY)
+    const result = await articleTable.get_row_by_id(id)
+    return result ?? null
 }
+
+export async function get_articles_by_article_id_issue(id: string): Promise<Article[] | null> {
+    const articleTable = new SupabaseTable<Article>('article', API_URL, API_KEY);
+
+    const targetArticle = await articleTable.get_row_by_id(id);
+    if (!targetArticle || !targetArticle.issue_id) {
+        return null;
+    }
+
+    const allInSameIssue = await articleTable.get_data({ issue_id: targetArticle.issue_id });
+    return allInSameIssue;
+}
+
+
